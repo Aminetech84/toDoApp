cdee74e20c6bde32f286d53a29efe13a4e9ba22e
Amine Ouldaouali <aminetech84@hotmail.com>
2024-10-14 17:05:57 +0100

    Update fetching using try/catch and add error handler

--- pro/frontend/src/App.js
+++ pro/frontend/src/App.js
@@ -1,29 +1,61 @@
-import {useState, useEffect} from 'react'
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([{
-    text: '',
-    completed: false
-  }]);
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    fetch('/api/tasks').then(res => {
-      if (res.ok) {
-        console.log(res);
-        return res.json();
-
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch("/api/tasks");
+        const data = await response.json();
+        setTasks(data);
+      } catch (error) {
+        console.error("Error fetching tasks", error);
       }
-    }).then(jsonRes => setTasks(jsonRes))
-    
-  });
+    };
+    fetchTasks();
+  }, []);
+
+  
+
+
+
   return (
     <div className="App">
-      {tasks.map(task => {
-        return <h1>{task.text}</h1>
+      {tasks.map((task) => {
+        return <h1>{task.text}</h1>;
       })}
     </div>
   );
 }
 
 export default App;
+
+/*
+
+const [tasks, setTasks] = useState([{
+  text: '',
+  completed: false
+}]);
+
+useEffect(() => {
+  fetch('/api/tasks').then(res => {
+    if (res.ok) {
+      console.log(res);
+      return res.json();
+
+    }
+  }).then(jsonRes => setTasks(jsonRes))
+  
+});
+return (
+  <div className="App">
+    {tasks.map(task => {
+      return <h1>{task.text}</h1>
+    })}
+  </div>
+);
+
+
+*/
