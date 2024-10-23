e72461e078ebf325d1edb90e2ea0fe2c980c9359
Amine Ouldaouali <aminetech84@hotmail.com>
2024-10-23 07:17:41 +0100

    2nd commit

--- pro/backend/index.js
+++ pro/backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require('dotenv');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,13 +9,14 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 // MongoDB connection
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
+  
 // Define task schema
 const taskSchema = new mongoose.Schema({
   text: String,
