d018471574f2bf2e4720e1fdc93a7663a1961fb9
Amine Ouldaouali <aminetech84@hotmail.com>
2024-10-27 05:42:13 +0100

    Add env file and config in mid folder

--- mid/server/index.js
+++ mid/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,12 +9,11 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 // MongoDB connection
 mongoose
-  .connect(
-    "mongodb+srv://aminetech84:Azer1st628@cluster0.tptbq.mongodb.net/Tasks"
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
@@ -86,7 +85,5 @@ app.delete("/api/tasks/:id", async (req, res) => {
 
 // Listening to the server
 app.listen(port, () => console.log(`Server listening on port ${port}`));
-
-
 
 //module.exports = index;
